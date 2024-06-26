5b490cbe548abb3e4353b2364b59ed60fd7eb527
ahmed <ahmed.ali@diracsystems.com>
2024-06-26 17:22:59 +0300

    add database connection

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const dotenv = require('dotenv').config();
+
+const connectDB = require('./db/connect');
+
+const PORT = process.env.PORT || 5000;
 
 const app = express()
-const port = 5000
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGODB_URL);
+    app.listen(PORT, () =>
+      console.log(`Server is listening on port ${PORT}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
